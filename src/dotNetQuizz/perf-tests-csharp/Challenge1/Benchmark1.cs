9f767cdb6b4507471ea3ecc801a3358bfd4b041e
Jaskula Tomasz <thomasv1000@hotmail.fr>
2017-05-25 01:12:23 +0200

    Refactoring to cleaner output names

--- src/dotNetQuizz/perf-tests-csharp/Challenge1/Benchmark1.cs
+++ src/dotNetQuizz/perf-tests-csharp/Challenge1/Benchmark1.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Columns;
 using BenchmarkDotNet.Attributes.Exporters;
 
 namespace perf_tests_csharp.Challenge1
 {
     [MarkdownExporter, AsciiDocExporter, HtmlExporter, CsvExporter, RPlotExporter]
+    [MinColumn, MaxColumn]
     public class Benchmark1
     {
         private Random _rdn;
@@ -15,22 +17,22 @@ namespace perf_tests_csharp.Challenge1
         private List<int> _items2;
 
         [Params(10, 20, 200, 2000, 20000, 200000, 2000000)]
-        public static int Range { get; set; }
+        public static int NbElements { get; set; }
 
         [Setup]
         public void SetupBenchmark1()
         {
-            var range = Enumerable.Range(0, Range).ToList();
+            var range = Enumerable.Range(0, NbElements).ToList();
             _items1 = new HashSet<int>(range);
             _items2 = new List<int>(range);
             _rdn = new Random();
-            _value = _rdn.Next(0, (int)(Range * 2.5));
+            _value = _rdn.Next(0, (int)(NbElements * 2.5));
         }
 
         [Benchmark]
-        public void BenchmarkLeft() => Left.Run(_value, _items1);
+        public void HashSet() => Left.Run(_value, _items1);
 
         [Benchmark]
-        public void BenchmarkRight() => Right.Run(_value, _items2);
+        public void List() => Right.Run(_value, _items2);
     }
 }
