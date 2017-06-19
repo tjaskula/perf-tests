d9e27c718a4795e9f697e0cebe1d75597bffc909
Jaskula Tomasz <thomasv1000@hotmail.fr>
2017-06-19 10:15:14 +0200

    Add benchamrk5 for Challenge5

---
+++ src/perf-tests-csharp/Challenge5/Left.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace perf_tests_csharp.Challenge5
+{
+    public class Left
+    {
+        public static void Run(int value, List<int> items)
+        {
+            MoveToFirst(value, items);
+        }
+
+        private static void MoveToFirst(int value, List<int> items)
+        {
+            var valueIndex = items.IndexOf(value);
+            if (valueIndex < 0)
+                return;
+
+            items.RemoveAt(valueIndex);
+            items.Insert(0, value);
+        }
+    }
+}
