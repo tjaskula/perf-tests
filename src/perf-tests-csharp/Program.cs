c5b69695a5e50388ad11f5cbeb0ddc6065051127
Jaskula Tomasz <thomasv1000@hotmail.fr>
2017-06-19 10:16:30 +0200

    Add benchmark5 to initialization

--- src/perf-tests-csharp/Program.cs
+++ src/perf-tests-csharp/Program.cs
@@ -4,6 +4,7 @@ using perf_tests_csharp.Challenge2;
 using perf_tests_csharp.Challenge2bis;
 using perf_tests_csharp.Challenge3;
 using perf_tests_csharp.Challenge4;
+using perf_tests_csharp.Challenge5;
 
 namespace perf_tests_csharp
 {
@@ -16,7 +17,8 @@ namespace perf_tests_csharp
                 typeof(Benchmark2),
                 typeof(Benchmark2bis),
                 typeof(Benchmark3),
-                typeof(Benchmark4)
+                typeof(Benchmark4),
+                typeof(Benchmark5)
             });
             switcher.Run(args);
         }
