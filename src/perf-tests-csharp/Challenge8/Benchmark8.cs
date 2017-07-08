c4b2b4ddf4f74c0baca24ddb5dab4e95357a7f93
Jaskula Tomasz <thomasv1000@hotmail.fr>
2017-07-08 17:44:06 +0200

    Add starters for Challenge8

---
+++ src/perf-tests-csharp/Challenge8/Benchmark8.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Columns;
+using BenchmarkDotNet.Attributes.Exporters;
+
+namespace perf_tests_csharp.Challenge8
+{
+    [MarkdownExporter, AsciiDocExporter, HtmlExporter, CsvExporter, RPlotExporter]
+    [MinColumn, MaxColumn]
+    public class Benchmark8
+    {
+        private static int[][] _matrix;
+
+        [Params(10, 20, 200, 2000, 20000, 200000, 2000000)]
+        public static int NbRows { get; set; }
+
+        [Setup]
+        public void SetupBenchmark8()
+        {
+            _matrix = Enumerable.Range(0, NbRows)
+                .Select(_ => Enumerable.Range(0, NbRows * 250).ToArray())
+                .ToArray();
+        }
+
+        [Benchmark]
+        public void RowEnumerate() => Left.Run(_matrix, NbRows, NbRows * 250);
+
+        [Benchmark]
+        public void ColumnEnumerate() => Right.Run(_matrix, NbRows, NbRows * 250);
+    }
+}
---
+++ src/perf-tests-csharp/Challenge8/Left.cs
@@ -0,0 +1,17 @@
+﻿namespace perf_tests_csharp.Challenge8
+{
+    public class Left
+    {
+        public static void Run(int[][] matrix, int rowCount, int columnCount)
+        {
+            var sum = 0;
+            for (var row = 0; row < rowCount; row++)
+            {
+                for (var column = 0; column < columnCount; column++)
+                {
+                    sum += matrix[row][column];
+                }
+            }
+        }
+    }
+}
