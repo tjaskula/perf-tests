cf812c2c56482b6a6f9c55d462bed0c1766f0e0c
Jaskula Tomasz <thomasv1000@hotmail.fr>
2017-07-23 13:01:17 +0200

    Add challenge10

---
+++ src/perf-tests-csharp/Challenge10/Benchmark10.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Columns;
+using BenchmarkDotNet.Attributes.Exporters;
+
+namespace perf_tests_csharp.Challenge10
+{
+    [MarkdownExporter, AsciiDocExporter, HtmlExporter, CsvExporter, RPlotExporter]
+    [MinColumn, MaxColumn]
+    public class Benchmark10
+    {
+        private Random _rdn;
+        private int _value;
+
+        [Setup]
+        public void SetupBenchmark10()
+        {
+            _rdn = new Random();
+            _value = _rdn.Next();
+        }
+
+        [Benchmark]
+        public void List() => Left.Run(_value);
+
+        [Benchmark]
+        public void LinkedList() => Right.Run(_value);
+    }
+}
---
+++ src/perf-tests-csharp/Challenge10/Right.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+
+namespace perf_tests_csharp.Challenge10
+{
+    public class Right
+    {
+        public static void Run(int value)
+        {
+            var isValid = IsValid1(value) & IsValid2(value) & IsValid3(value);
+        }
+
+        private static bool IsValid1(int n)
+        {
+            return n < 100;
+        }
+
+        private static bool IsValid2(int n)
+        {
+            return n % 10 == 0;
+        }
+
+        private static bool IsValid3(int n)
+        {
+            Thread.Sleep(1);
+            return n % 3 == 0;
+        }
+    }
+}
