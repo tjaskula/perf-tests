f34f4210f7896164dfb326ac4f4aece334a7ec77
Jaskula Tomasz <thomasv1000@hotmail.fr>
2017-08-03 17:12:48 +0200

    Add chellenge12

---
+++ src/perf-tests-csharp/Challenge12/Benchmark12.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Columns;
+using BenchmarkDotNet.Attributes.Exporters;
+
+namespace perf_tests_csharp.Challenge12
+{
+    [MarkdownExporter, AsciiDocExporter, HtmlExporter, CsvExporter, RPlotExporter]
+    [MinColumn, MaxColumn]
+    public class Benchmark12
+    {
+        private Random _rdn;
+        private List<int> _items1;
+        private LinkedList<int> _items2;
+        private int _value;
+
+        [Params(10, 20, 200, 2000, 20000, 200000, 2000000)]
+        public static int NbElements { get; set; }
+
+        [Setup]
+        public void SetupBenchmark12()
+        {
+            var values = Enumerable.Range(0, NbElements).ToList();
+            _rdn = new Random();
+            _value = _rdn.Next(0, NbElements);
+            _items1 = values;
+            _items2 = new LinkedList<int>(values);
+        }
+
+        [Benchmark]
+        public void List() => Left.Run(_value, _items1);
+
+        [Benchmark]
+        public void LinkedList() => Right.Run(_value, _items2);
+    }
+}
