46b847ce9ca5eb5fd0ebd0ffbe55a6e1b24fed55
Jaskula Tomasz <thomasv1000@hotmail.fr>
2017-08-29 10:13:02 +0200

    Fixing some errors in Benchmark15

--- src/perf-tests-csharp/Challenge15/Benchmark15.cs
+++ src/perf-tests-csharp/Challenge15/Benchmark15.cs
@@ -10,7 +10,7 @@ namespace perf_tests_csharp.Challenge15
     [MinColumn, MaxColumn]
     public class Benchmark15
     {
-        private List<int> _values;
+        private List<int> _values = new List<int>();
 
         [Params(10, 20, 200, 500, 2000, 20000, 200000, 2000000)]
         public static int NbElements { get; set; }
@@ -18,13 +18,14 @@ namespace perf_tests_csharp.Challenge15
         [Setup]
         public void SetupBenchmark15()
         {
-            _values = Enumerable.Range(0, NbElements).ToList();
+            _values.Clear();
+            _values.AddRange(Enumerable.Range(0, NbElements));
         }
 
         [Benchmark]
-        public void List() => Left.Run(_values);
+        public void Left() => Challenge15.Left.Run(_values);
 
         [Benchmark]
-        public void LinkedList() => Right.Run(_values);
+        public void Right() => Challenge15.Right.Run(_values);
     }
 }
